5cae5bb7571b84d7e1f02cfd048b3c8309753e4d
Miles Budnek <mbudnek@mbudnek.com>
2021-01-31 17:50:05 -0500

    Avoid breaking the whole app because of a bad toggle (#55)
    When creating a new toggle in the Toggles trait it's possible to
    leave the page without filling in any labels by either closing the
    browser or clicking a link out of the app.  This would break the
    entire app settings due to an error trying to load that bad toggle.
    
    This adds a default "Unknown" label that will be used in that case
    so that the app will continue to function and the user can delete
    the bad toggle.

--- google-home-community.groovy
+++ google-home-community.groovy
@@ -44,6 +44,7 @@
 //   * Oct 03 2020 - Add support for devices not allowing volumeSet command when changing volume
 //   * Jan 18 2021 - Fix SetTemperature command of the TemperatureControl trait
 //   * Jan 19 2021 - Added Dock and StartStop Traits
+//   * Jan 31 2021 - Don't break the whole app if someone creates an invalid toggle
 
 import groovy.json.JsonException
 import groovy.json.JsonOutput
@@ -2834,6 +2835,9 @@ private traitFromSettings_Toggles(traitName) {
             traitName: traitName,
             labels: settings."${toggle}.labels"?.split(",")
         ]
+        if (toggleAttrs.labels == null) {
+            toggleAttrs.labels = ["Unknown"]
+        }
         toggleAttrs << traitFromSettings_OnOff(toggle)
         toggleAttrs
     }
