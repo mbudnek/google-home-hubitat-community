6b7b58cae1a518e9a218714de9ba0de8ca86aa76
Miles Budnek <mbudnek@mbudnek.com>
2021-05-07 23:47:57 -0400

    Update copyright notice
    Update the year, and change it to correctly reflect that I am not
    the sole copyright holder for this code.

--- google-home-community.groovy
+++ google-home-community.groovy
@@ -1,4 +1,4 @@
-// Copyright 2020 Miles Budnek
+// Copyright 2021 The Google Home Community Contributors
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
