mbudnek/google-home-hubitat-community

commit 6c155a46bbeb4363081011d387be1241d2eff965

Tolerate attempting to round null (#90)
There was a NullPointerException reported from passing null into the
roundTo method.  I'm not exactly sure what's null that's being passed
in, so this makes the method tolerate the null and generate a stack
trace so I can track it down.

google-home-community.groovy
@@ -70,6 +70,7 @@
 //                 - Added support for returning the matching user position for Arm/Disarm and Lock/Unlock to the device driver
 //   * Jun 21 2022 - Apply rounding more consistently to temperatures
 //   * Jun 21 2022 - Added SensorState Trait
+//   * Jun 23 2022 - Fix error attempting to round null
 
 import groovy.json.JsonException
 import groovy.json.JsonOutput
@@ -4588,7 +4589,12 @@ private allKnownDevices() {
 
 private roundTo(number, decimalPlaces) {
     def factor = Math.max(1, 10 * decimalPlaces)
-    return Math.round(number * factor) / factor
+    try {
+        return Math.round(number * factor) / factor
+    catch (NullPointerException e) {
+        LOGGER.exception("Attempted to round null!", e)
+        return null
+    }
 }
 
 private googlePercentageToHubitat(percentage) {

commit 59124044012a92e4ba670f1485da3fbfb1646b15

Fix syntax error introduced in #90

google-home-community.groovy
@@ -4591,7 +4591,7 @@ private roundTo(number, decimalPlaces) {
     def factor = Math.max(1, 10 * decimalPlaces)
     try {
         return Math.round(number * factor) / factor
-    catch (NullPointerException e) {
+    } catch (NullPointerException e) {
         LOGGER.exception("Attempted to round null!", e)
         return null
     }