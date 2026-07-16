0ffb421b86eecff6cfe101e7ed49c2ae9c9371a7
Rasmus Christiansen <rasmus.christiansen@gmail.com>
2026-07-16 10:43:51 +0200

    Optimized updated() to only call allKnownDevices() once (#121)
    Optimized updated() to only call allKnownDeviced() once
    
    ---------
    
    Co-authored-by: Miles Budnek <mbudnek@gmail.com>

--- google-home-community.groovy
+++ google-home-community.groovy
@@ -82,6 +82,7 @@
 //   * Jun 06 2023 - Add support for the OccupancySensing trait
 //   * Apr 12 2024 - Code cleanup
 //   * May 08 2026 - Move to the new "Integrations" menu
+//   * Jun 16 2026 - Optimize 'updated' method to only call 'allKnownDevices' once
 
 import groovy.json.JsonException
 import groovy.json.JsonOutput
@@ -139,10 +140,11 @@ def updated() {
     if (settings.googleServiceAccountJSON) {
         requestSync()
         if (settings.reportState) {
-            allKnownDevices().each { entry ->
+            def devices = allKnownDevices()
+            devices.each { entry ->
                 subscribe(entry.value.device, "handleDeviceEvent", [filterEvents: true])
             }
-            reportStateForDevices(allKnownDevices())
+            reportStateForDevices(devices)
         }
     }
 }
